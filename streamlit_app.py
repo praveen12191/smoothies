a6f51315ff627614c7d2fc1a888d5b5a36e98a3e
Praveen R <76170116+praveen12191@users.noreply.github.com>
2024-05-21 16:40:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,9 @@ session = cns.session()
 
 # Fetch fruit options from Snowflake
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('search_on')).collect()
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 # Create a multiselect widget for fruit options
 data = st.multiselect('data', [row['FRUIT_NAME'] for row in my_dataframe], max_selections=5)
