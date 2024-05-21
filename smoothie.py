2ae1bdf058b191f2d2a5a536b86ef86a4d1bf2cf
Praveen R <76170116+praveen12191@users.noreply.github.com>
2024-05-21 11:30:04 +0530

    create smoothie.py

---
+++ smoothie.py
@@ -0,0 +1,31 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+
+
+from snowflake.snowpark.context import get_active_session
+
+# Write directly to the app
+st.title("Example Streamlit App :balloon:")
+
+
+name_on_order = st.text_input("Movie title")
+st.write("The current movie title is", name_on_order)
+
+# Get the current credentials
+session = get_active_session()
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+data = st.multiselect('data',my_dataframe,max_selections=5)
+st.write(data)
+if(data):
+    str = ''
+    for i in data:
+        str+=i+' '
+    st.write(str)
+    insert_query = """ insert into smoothies.public.orders(INGREDIENTS,
+	NAME_ON_ORDER) values('"""+str+"""','"""+name_on_order+"""')"""
+    st.write(insert_query)
+    session.sql(insert_query).collect()
+    st.success('Your Smoothie is ordered! '+name_on_order, icon="✅")
